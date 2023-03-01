606c95b58e9f59327133270875bce4e9fc84c908
xudefu <1152699620@qq.com>
2023-03-01 21:50:39 +0800

    vim fzf插件 shell配置,需要放置在$HOME目录
    Signed-off-by: xudefu <1152699620@qq.com>

---
+++ .fzf.zsh
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/xudefu/.vim/plugin/fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/home/xudefu/.vim/plugin/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/xudefu/.vim/plugin/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/xudefu/.vim/plugin/fzf/shell/key-bindings.zsh"
