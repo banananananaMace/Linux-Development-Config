b78671388b2249b1307f899201bf37c9065923da
xudefu <1152699620@qq.com>
2023-04-21 22:31:11 +0800

    添加自动配置文件

---
+++ config.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+# vim fzf 安装
+cd ~/.vim/plugin/fzf/ && ./install
+cd ~
+
+# git 配置
+git config --add oh-my-zsh.hide-status 1
+git config --add oh-my-zsh.hide-dirty 1
+git config --global user.name xudefu
+git config --global user.email 1152699620@qq.com
+git config --global init.defaultBranch master
+git config --global core.editor vim
+
+#coredump 配置
+ulimit -c unlimited
+su - root << EOF
+echo "core-%p-%e" > /proc/sys/kernel/core_pattern
+EOF
+
