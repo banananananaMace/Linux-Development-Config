e6ea7b8c2fc2e9b9563f841ea1253dab81d258f6
xudefu <1152699620@qq.com>
2023-04-23 23:43:49 +0800

    zsh插件安装

--- config.sh
+++ config.sh
@@ -24,3 +24,7 @@ ulimit -c unlimited
 # echo "core-%p-%e" > /proc/sys/kernel/core_pattern
 # EOF
 
+# zsh插件
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
