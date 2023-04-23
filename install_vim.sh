bd225c54d18021844e6663824cbb4e5e91da86da
banananananaMace <82626291+banananananaMace@users.noreply.github.com>
2023-04-23 14:47:44 +0800

    vim安装脚本增加彻底卸载vim功能

--- install_vim.sh
+++ install_vim.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 
+# 彻底卸载vim
+if which apt-get >/dev/null; then
+sudo apt-get autoremove -y vim vim-common vim-tiny vim-runtime gvim
+elif which yum >/dev/null; then
+sudo yum remove -y vim-runtime vim-common.x86_64 vim-filesystem.noarch vim-X11.x86_64 gvim vim-clustershell
+fi
+
 # 安装依赖库
 if which apt-get >/dev/null; then
-sudo apt-get install -y libncurses5-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev ruby-dev mercurial
+sudo apt-get install -y libncurses5-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev ruby-dev mercurial gvim
 elif which yum >/dev/null; then
-sudo yum install -y libncurses5-devel
-sudo yum install -y libgnome2-devel
-sudo yum install -y libgnomeui-devel
-sudo yum install -y libgtk2.0-devel
-sudo yum install -y libatk1.0-devel
-sudo yum install -y libbonoboui2-devel
-sudo yum install -y libcairo2-devel
-sudo yum install -y libx11-devel
-sudo yum install -y libxpm-devel
-sudo yum install -y libxt-devel
-sudo yum install -y python3-devel                                                                                                                                             
-sudo yum install -y ruby-devel
-sudo yum install -y mercurialel
+sudo yum install -y libncurses5-devel libgnome2-devel libgnomeui-devel libgtk2.0-devel libatk1.0-devel libcairo2-devel libx11-devel libxpm-devel libxpm-devel libxt-devel python3-devel ruby-devel mercurialel gvim
 fi
 
 
 git clone git@gitee.com:wennan_w/vim82.git ~
 cd ~/vim*/src
 sudo ./configure --with-features=huge --enable-python3interp=yes --with-python-config-dir=/usr/bin/python3.6m-x86_64-config -enable-gui=gtk2 --enable-cscope --prefix=/usr/local
+# VIMRUNTIMEDIR syntax.vim 等依赖文件安装，需要与/usr/local/share/vim/路径下的文件名一致
 sudo make VIMRUNTIMEDIR=/usr/local/share/vim/vim82
 sudo make install
-sudo ln -sf /usr/local/share/vim/vim82/bin/vim /usr/bin/vim
+sudo ln -sf /usr/local/bin/vim /usr/bin/vim
