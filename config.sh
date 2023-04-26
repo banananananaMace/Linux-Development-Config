d3e056e8b684598abe166132716108f9e91a74d9
xudefu <1152699620@qq.com>
2023-04-26 21:19:10 +0800

    删除个人隐私信息

--- config.sh
+++ config.sh
@@ -12,8 +12,8 @@ cp ~/.vim/plugged/YouCompleteMe/third_party/ycmd/.ycm_extra_conf.py ~
 # git 配置
 git config --add oh-my-zsh.hide-status 1
 git config --add oh-my-zsh.hide-dirty 1
-git config --global user.name xudefu
-git config --global user.email 1152699620@qq.com
+git config --global user.name banananaMace
+git config --global user.email 421894173@qq.com
 git config --global init.defaultBranch master
 git config --global core.editor vim
 git config --global color.ui true
