453b6c63787c7460b8206641843d2cb0bfc5ff46
banananaMace <421894173@qq.com>
2024-03-10 11:07:35 +0800

    修改tmux安装脚本
    Signed-off-by: banananaMace <421894173@qq.com>

--- tmux/install_tmux.sh
+++ tmux/install_tmux.sh
@@ -2,7 +2,7 @@
 # tmux_download_installer.sh
 # download
 # read -p "Enter the Download path: " target_path
-read -p "Enter the install path: " install_path
+# read -p "Enter the install path: " install_path
 
 # mkdir ${install_path}
 
@@ -14,25 +14,29 @@ read -p "Enter the install path: " install_path
 # sudo tar -xzvf ${target_path}/libevent-2.1.11-stable.tar.gz -C ${install_path}
 # sudo tar -xzvf ${target_path}/ncurses-6.2.tar.gz -C ${install_path}
 # sudo tar -xzvf ${target_path}/tmux-3.0a.tar.gz -C ${install_path}
-sudo tar -xzvf libevent-2.1.11-stable.tar.gz -C ${install_path}
-sudo tar -xzvf ncurses-6.2.tar.gz -C ${install_path}
-sudo tar -xzvf tmux-3.0a.tar.gz -C ${install_path}
+# sudo tar -xzvf libevent-2.1.11-stable.tar.gz -C ${install_path}
+# sudo tar -xzvf ncurses-6.2.tar.gz -C ${install_path}
+# sudo tar -xzvf tmux-3.0a.tar.gz -C ${install_path}
+sudo tar -xzvf tmux-3.0a.tar.gz -C /usr/bin
 
 #ncurses
-cd ${install_path}/ncur*
-sudo ./configure --prefix=/usr/bin
-sudo make && sudo make install
+# cd ${install_path}/ncur*
+# sudo ./configure --prefix=/usr/bin
+# sudo make && sudo make install
 
 #libbevent
-cd ${install_path}/libevent*
-sudo ./configure --prefix=/usr/bin --disable-shared
-sudo make && sudo make install
-sudo ln -s /usr/lib64/libevent-*.so.* /usr/lib64/libevent.so
+# cd ${install_path}/libevent*
+# sudo ./configure --prefix=/usr/bin --disable-shared
+# sudo make && sudo make install
+# sudo ln -s /usr/lib64/libevent-*.so.* /usr/lib64/libevent.so
 # sudo yum install -y libevent-dev
 # sudo yum install libevent -y
 
 #tmux
-cd ${install_path}/tmux*
+sudo apt install -y libevent-dev
+sudo apt install -y libncurses5-dev
+
+cd /usr/bin/tmux*
 sudo ./configure --prefix=/usr/bin
 sudo make && sudo make install
 #CPPFLAGS="-I/my/path/tmux_depend//include -I/my/path/tmux_depend//include/ncurses" LDFLAGS="-static -L/my/path/tmux_depend//include -L/my/path/tmux_depend//include/ncurses -L/my/path/tmux_depend//lib"
