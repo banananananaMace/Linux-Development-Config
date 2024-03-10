27521eaf30b44102fba6679cd2fb25b1f083648a
banananaMace <421894173@qq.com>
2024-03-10 21:19:27 +0800

    libasan内存越界检测工具安装
    Signed-off-by: banananaMace <421894173@qq.com>

--- gcc_11.4.0/install_gcc.sh
+++ gcc_11.4.0/install_gcc.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #!/bin/bash
 # 下载 GCC 源代码
+sudo apt-get update
+sudo apt-get install autoconf automake libtool
+
 wget https://mirrors.aliyun.com/gnu/gcc/gcc-11.4.0/gcc-11.4.0.tar.gz
 tar -xzvf gcc-11.4.0.tar.gz
 cd gcc-11.4.0
@@ -16,3 +19,15 @@ for i in $(ls /usr/local/gcc-11/bin); do
     test -f /usr/bin/$i && (mv /usr/bin/$i /usr/bin/$i.$(date +"%Y-%m-%dT%H:%M:%S") && ln -s /usr/local/gcc-11/bin/$i /usr/bin/$i || exit 21)
 done
 echo "GCC 11.4.0 已成功安装！"
+
+# libasan安装
+gcc_dir=$(pwd)
+cd ${gcc_dir}/libstdc++-v3
+./configure
+sudo make && sudo make install
+cd ${gcc_dir}/libsanitizer
+./configure
+sudo make && sudo make install
+
+echo "/usr/local/lib/../lib64" | sudo tee -a /etc/ld.so.conf
+sudo ldconfig
